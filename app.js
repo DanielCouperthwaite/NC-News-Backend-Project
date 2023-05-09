d1b3f22005d31415fdbb3cca458d3083f0068a79
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-05-09 15:44:33 +0100

    Submitting corrections for task 3

--- app.js
+++ app.js
@@ -10,4 +10,11 @@ app.all('*', (req, res) => {
     res.status(404).send({ msg: 'Oh no! Please enter a valid url' })
 });
 
+app.use((err, req, res, next,) => {
+    if(err.status && err.msg){
+        res.status(err.status).send({msg: err.msg})
+    }
+})
+
+
 module.exports = app;
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -5,5 +5,8 @@ exports.getTopics = (req, res) => {
     fetchTopics().then((topics) => {    
         res.status(200).send({topics})
     })
+    .catch((err) => {
+        next(err);
+      });
 }
 
