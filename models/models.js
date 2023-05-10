83a603f812deb8905e9dd40b8b3061e2fc4f7a8d
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-05-10 10:13:31 +0100

    Submitting changes to completed ticket 3.5

--- models/models.js
+++ models/models.js
@@ -1,6 +1,4 @@
 const connection = require('../db/connection')
-const fs = require('fs/promises')
-// const endpoints = require('../endpoints.json')
 
 exports.fetchTopics = () => {
     console.log('in model')
@@ -8,10 +6,3 @@ exports.fetchTopics = () => {
         return result.rows;
     })
 }
-
-// exports.fetchApiInfo = () => {
-//     console.log('in model')
-//     return fs.readFile(endpoints).then((result) => {    
-//         return result;
-//     })
-// }
