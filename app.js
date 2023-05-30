ff96640318e4a524727ef036542c400a79433883
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-05-30 11:00:42 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 
 const {getTopics, getApiInfo, getArticleById, getComments, getAllArticles, postComment, patchArticle} = require('./controllers/controllers')
+
+app.use(cors());
 
 app.use(express.json())
 
