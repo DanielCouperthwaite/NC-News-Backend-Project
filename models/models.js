1d27334ff1bab7cedb0a7667db5f0a624e8c6d6f
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-08-01 07:24:34 +0100

    simplify fetch topics model

--- models/models.js
+++ models/models.js
@@ -8,7 +8,6 @@ exports.fetchTopics = () => {
 }
 
 exports.fetchArticle = (id) => {
-    
     return connection.query(`SELECT * FROM articles WHERE article_id = $1;`, [id]).then((result) => {
         if(result.rows.length === 0){
             return Promise.reject({status: 404, msg: "Oh no! Please enter a valid article ID!"});
